a0708b8c507cd9a3f5188ee1d56e612945b72edf
Nevzat <nevzatcelik151907@gmail.com>
2023-01-30 17:09:01 +0100

    27.01

---
+++ src/test/java/day01/day01.java
@@ -0,0 +1,50 @@
+package day01;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class day01 {
+    WebDriver driver;
+    String https="https://www.";
+
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+         driver=new ChromeDriver();
+         driver.manage().window().maximize();
+         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+
+
+
+    @Test
+    public void test01(){
+        driver.get(https+"google.com");
+        driver.navigate().to(https+"amazon.com");
+        driver.navigate().refresh();
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.navigate().to(https+"facebook.com");
+     //   driver.switchTo().frame("webelement").switchTo();
+
+
+
+
+
+    }
+
+
+    @After
+    public void tearDown(){
+     // driver.close();
+    }
+}
---
+++ src/test/java/day02/day02.java
@@ -0,0 +1,55 @@
+package day02;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class day02 {
+    /*
+    1. Launch browser
+    2. Navigate to url 'http://automationexercise.com'
+    3. Verify that categories are visible on left side bar
+
+     */
+    WebDriver driver;
+    String https="https://www.";
+
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @Test
+    public void exerciseTest() throws InterruptedException {
+         //  1. Launch browser
+        //    2. Navigate to url 'http://automationexercise.com'
+        driver.get(https+"automationexercise.com");
+        //3. Verify that categories are visible on left side bar
+        WebElement categoryYazisi= driver.findElement(By.xpath("//h2[text()='Category']"));
+        Assert.assertTrue(categoryYazisi.isDisplayed());
+        //  4. Click on 'Women' category
+        driver.findElement(By.xpath("//a[@href='/products']")).click();
+        Thread.sleep(2000);
+        //reklamin
+      //  driver.findElement(By.xpath("//div[@id='dismiss-button']")).click();
+        WebElement womenButton= driver.findElement(By.xpath("//a[@href='#Women']"));
+        womenButton.click();
+
+    }
+
+    @After
+    public void tearDown(){
+        //driver.close();
+    }
+}
